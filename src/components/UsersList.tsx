9315b3b924470d540e91f2abc6cbec3ad96c25bc
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2023-05-16 23:33:48 +0600

    created hook UseTypedSelector

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import {useTypedSelector} from "../hooks/useTypedSelector";
 
 const UsersList: React.FC = () => {
-    const state = useSelector(state => state.users)
+    const {users, loading, error} = useTypedSelector(state => state.users)
     return (
         <div>
 
