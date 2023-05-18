1f64952752bd177c417c0f93e6f0fb3046d9efbd
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2023-05-18 23:38:08 +0600

    Asynchronous action creator + typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
     <div >
-
+      <UsersList/>
     </div>
   );
 }
--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import {useSelector} from "react-redux";
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../store/action-creators/user";
+import type {} from 'redux-thunk/extend-redux';
 
 const UsersList: React.FC = () => {
     const {users, loading, error} = useTypedSelector(state => state.users)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if(loading) {
+        return <h1>Идет загрузка...</h1>
+    }
+    if(error) {
+        return <h1>{error}</h1>
+    }
+
     return (
         <div>
-
+            {users.map( user =>
+                <div key={user.id}>{user.name}</div>
+            )}
         </div>
     );
 };
 
-export default UsersList: React.FC;
+export default UsersList;
